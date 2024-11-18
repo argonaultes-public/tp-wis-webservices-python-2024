4d8d6f4e6458aa40e9cdb37ffcb506847906527d
argonaultes <gael@argonaultes.fr>
2024-11-18 14:51:58 +0100

    exo2 ok

--- exo2bis.py
+++ exo2bis.py
@@ -13,27 +13,22 @@ for _ in range(nb_max_spaceships):
     nb_seats = int(input('nb de places: '))
     name = input('nom: ')
 
-    spaceship = {
-        'name': name,
-        'color': color,
-        'nb_passengers': nb_passengers,
-        'nb_seats': nb_seats,
-        'ratio_passenger': nb_passengers / nb_seats}
+    spaceship = (name, color, nb_passengers, nb_seats, nb_passengers / nb_seats)
     hangar.append(spaceship)
 
-    if best_ratio < spaceship['ratio_passenger']:
-        best_ratio = spaceship['ratio_passenger']
+    if best_ratio < spaceship[-1]:
+        best_ratio = spaceship[-1]
         best_vessel = spaceship
 
 print(f'V1: {best_vessel}')
 
 # solution 2
 def get_ratio_passenger(spaceship):
-    return spaceship['ratio_passenger']
+    return spaceship[-1]
 
 result2 = max(hangar, key = get_ratio_passenger)
 
-result1 = max(hangar, key = lambda spaceship : spaceship['ratio_passenger'])
+result1 = max(hangar, key = lambda spaceship : spaceship[-1])
 
 # hangar[idx] ?? => 
 
