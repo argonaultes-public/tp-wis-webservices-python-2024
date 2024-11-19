823b5bdb69c5f23d7bf00bdf93b0b9448f2b1652
argonaultes <gael@argonaultes.fr>
2024-11-19 10:07:13 +0100

    exo3 ok on python 3.13

--- exo3.py
+++ exo3.py
@@ -14,16 +14,25 @@ def get_image_wis():
     url_image = 'http://safaridesmetiers.tech/wp-content/uploads/2021/08/Epsi-Wiz-Format-page-Partenaires.png'
 
     response = requests.get(url_image)
-    if response.status_code:
+    if response.status_code == 200:
         binary_content = response.content
         with open(file = './image.png', mode = 'wb') as f:
             f.write(binary_content)
 
 def get_tasks():
-    pass
+    # query web service tasks
+    url_api_tasks = 'https://my-json-server.typicode.com/argonaultes-public/tp-wis-webservices-python-2024/tasks'
+    response = requests.get(url_api_tasks)
 
-# query web service tasks
-
+    response.raise_for_status()
+    if response.status_code == 200:
+        print(response.content)
+        tasks = response.json()
+        print(tasks)
+        for task in tasks:
+            print(f'Tâche n°{task['id']}: {task['title'].capitalize()}')
+            
+get_tasks()
 # requests.
 
 # display all tasks
